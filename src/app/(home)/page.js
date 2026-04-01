f62a738beb622facb1f25dccba8c956f0e6d2297
QixiaoLiu0 <christal.liu008@gmail.com>
2026-03-31 22:25:14 -0600

    feat: DishCard (#1)

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,7 +1,18 @@
+import DishCard from "@/components/DishCard";
+
 export default function Page() {
   return (
     <main>
       <h1>home page</h1>
+      <br />
+      <div className="grid grid-cols-5 gap-10">
+        <DishCard />
+        <DishCard />
+        <DishCard />
+        <DishCard />
+        <DishCard />
+        <DishCard />
+      </div>
     </main>
   );
 }
---
+++ src/components/DishCard.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function DishCard({ imgUrl, dishName }) {
+  return (
+    <Link
+      href="#"
+      className="border-2 border-red-200 aspect-ratio: 1 / 1 overflow-hidden "
+    >
+      <div className="h-[90%] w-full">
+        <img
+          className="w-full h-full object-fit: cover"
+          src={imgUrl || "/kvbotn1581012881.jpg"}
+          alt={dishName || "placeholder"}
+        />
+      </div>
+      <div className="h-[10%] flex items-center justify-center">
+        <h2>{dishName || "dish name area..."}</h2>
+      </div>
+    </Link>
+  );
+}
